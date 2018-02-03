117c9845076f057ed152b4a2efbf6dbcbfb00522
Alex Bass <alexpjbass@gmail.com>
2018-02-03 17:49:48 -0500

    feat: Add loading spinner on login page

--- Login.js
+++ Login.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import url from 'url'
 import querystring from 'querystring'
-import { StyleSheet, Alert, KeyboardAvoidingView, Linking, AsyncStorage } from 'react-native'
+import { StyleSheet, View, ActivityIndicator, KeyboardAvoidingView, Linking, AsyncStorage } from 'react-native'
 import { clientID, clientSecret, callback, endpoint, scopes } from './secrets'
 import { TFHeading2 } from './components/typography'
 import { TFForm, TFButton } from './components/form-elements'
 
 export default class Login extends React.Component {
-  state = {}
+  state = {
+    loading: true
+  }
 
   async componentDidMount () {
     const token = await AsyncStorage.getItem('AccessToken')
     if (token) {
-      Alert.alert(`You're authenticated`)
+      this.goToListForms(token)
+    } else {
+      this.setState({ loading: false })
     }
   }
 
@@ -25,6 +29,7 @@ export default class Login extends React.Component {
     if (!code) {
       return
     }
+
     const body = querystring.stringify({
       'code': code,
       'client_id': clientID,
@@ -43,9 +48,10 @@ export default class Login extends React.Component {
 
     const json = await response.json()
 
+    const token = json['access_token']
     await AsyncStorage.setItem('AccessToken', json['access_token'])
 
-    Alert.alert(`You're authenticated`)
+    this.goToListForms(token)
   }
 
   _doAuthentication = () => {
@@ -54,15 +60,30 @@ export default class Login extends React.Component {
     Linking.addEventListener('url', this._handleOauthCallback)
   }
 
+  goToListForms (token) {
+    const { navigate } = this.props.navigation
+    navigate('ListForms', { token })
+  }
+
   render () {
+    if (this.state.loading) {
+      return (
+        <View style={styles.loadingContainer}>
+          <ActivityIndicator size='large' color='#000'/>
+        </View>
+      )
+    }
+
     return (
       <KeyboardAvoidingView
         behavior='padding'
         style={styles.container}
       >
-        <TFHeading2>
-          Read your typeform responses on the go.
-        </TFHeading2>
+        <View style={styles.headingWrap}>
+          <TFHeading2>
+            Read your typeform responses on the go.
+          </TFHeading2>
+        </View>
         <TFForm>
           <TFButton
             large
@@ -76,10 +97,18 @@ export default class Login extends React.Component {
 }
 
 const styles = StyleSheet.create({
+  loadingContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#f1ece4'
+  },
+  headingWrap: {
+    marginBottom: 24
   }
 })
