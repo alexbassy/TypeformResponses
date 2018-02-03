581118ca8f1fd9dec085a652dbbd76233c002e22
Alex Bass <alexpjbass@gmail.com>
2018-02-03 17:49:11 -0500

    feat: List forms

--- ListForms.js
+++ ListForms.js
@@ -1,57 +1,71 @@
 import React from 'react'
-import { Text, Image, View, StyleSheet } from 'react-native'
+import { endpoint } from './secrets'
+import { View, StyleSheet, ActivityIndicator, ScrollView } from 'react-native'
+import { List, ListItem } from 'react-native-elements'
 
-const people = { // A
-  0: {
-    name: 'Leela',
-    image: 'http://vignette1.wikia.nocookie.net/en.futurama/images/d/d4/Turanga_Leela.png/revision/latest?cb=20150218013044'
-  },
-  1: {
-    name: 'Bender',
-    image: 'https://vignette2.wikia.nocookie.net/en.futurama/images/4/43/Bender.png/revision/latest?cb=20150206072725'
-  },
-  2: {
-    name: 'Amy',
-    image: 'https://i.ytimg.com/vi/4sCtTq7K3yI/hqdefault.jpg'
-  },
-  3: {
-    name: 'Fry',
-    image: 'http://www.supergrove.com/wp-content/uploads/2017/03/fry-futurama-22-which-robot-from-quotfuturamaquot-are-you.jpg'
-  }
-}
-
-class People extends React.Component {
+export default class ListForms extends React.Component {
   static navigationOptions = {
-    title: 'People'
+    title: 'Your typeforms',
+    headerLeft: null
+  }
+
+  state = {
+    forms: []
+  }
+
+  componentDidMount () {
+    const { token } = this.props.navigation.state.params
+    this.getTypeforms(token)
+  }
+
+  async getTypeforms (token) {
+    console.log(token)
+    const response = await fetch(endpoint.listForms, {
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': `bearer ${token}`
+      }
+    })
+
+    const json = await response.json()
+
+    console.log(json)
+
+    this.setState({
+      forms: json.items
+    })
   }
 
   render () {
-    const { id } = this.props.navigation.state.params // B
-    if (!people[id]) {
-      return <Text>Sorry, no data exists for this user</Text>
+    const { forms } = this.state
+
+    if (!forms.length) {
+      return (
+        <View style={styles.loadingContainer}>
+          <ActivityIndicator size='large' color='#000'/>
+        </View>
+      )
     }
+
     return (
-      <View>
-        <Text style={styles.text}>{people[id].name}</Text>
-        <Image
-          resizeMode='contain'
-          style={styles.image}
-          source={{ uri: people[id].image }}
-        />
-      </View>
+      <ScrollView>
+        <List>
+          {this.state.forms.map((form, i) => (
+            <ListItem
+              key={i}
+              title={form.title}
+            />
+          ))}
+        </List>
+      </ScrollView>
     )
   }
 }
 
 const styles = StyleSheet.create({
-  text: {
-    margin: 19,
-    fontSize: 22
-  },
-  image: {
-    width: 400,
-    height: 400
+  loadingContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 })
-
-export default People
