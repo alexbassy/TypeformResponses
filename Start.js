0de578868bd7aca49c9f49aac7d9da04a0bfb5de
Alex Bass <alexpjbass@gmail.com>
2018-02-07 15:50:57 +0100

    chore: Start component instead of redirecting from Login

---
+++ Start.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { AsyncStorage } from 'react-native'
+import BaseComponent from './base'
+
+export default class Start extends BaseComponent {
+  async componentDidMount () {
+    const token = await AsyncStorage.getItem('AccessToken')
+    if (token) {
+      return this.skipLoginScreen()
+    } else {
+      return this.goToLoginScreen()
+    }
+  }
+
+  render () {
+    return null
+  }
+}
--- router.js
+++ router.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import { AppRegistry, Text } from 'react-native'
 import { StackNavigator } from 'react-navigation'
+import Start from './Start'
 import Login from './Login'
 import ListForms from './ListForms'
 import ViewResponses from './ViewResponses'
 
 const Router = StackNavigator({
+  Start: { screen: Start },
   Login: { screen: Login },
   ListForms: { screen: ListForms },
   ViewResponses: { screen: ViewResponses }
