c58b222002c2e8525d3a8d6062945c9a1531ac5e
Alex Bass <alexpjbass@gmail.com>
2018-02-12 17:22:53 +0100

    feat: Add function for sorting values by count

---
+++ __tests__/HorizontalBarChart.test.js
@@ -0,0 +1,16 @@
+import { sortByCount } from '../components/HorizontalBarChart'
+
+describe('sortByCount', () => {
+  it('should sort object their count property and return keys', function () {
+    const demoObj = {
+      Two: { count: 1 },
+      One: { count: 124 },
+      Three: { count: -99 },
+      Five: { count: 1 },
+      Six: { count: 10 },
+      Four: { count: 2 }
+    }
+
+    expect(sortByCount(demoObj)).toEqual(['One', 'Six', 'Four', 'Two', 'Five', 'Three'])
+  })
+})
--- components/HorizontalBarChart.js
+++ components/HorizontalBarChart.js
@@ -1,52 +1,67 @@
 import React from 'react'
 import { StyleSheet, View, Text, Image } from 'react-native'
 
-const HorizontalBarChart = ({ responses }) => {
+export const sortByCount = (obj) => {
+  const keys = Object.keys(obj)
+  return keys.sort((a, b) => {
+    if (obj[a].count > obj[b].count) {
+      return -1
+    } else if (obj[a].count < obj[b].count) {
+      return 1
+    }
+    return 0
+  })
+}
+
+const HorizontalBarChart = ({ responses, onLayout }) => {
+  const sortedKeys = sortByCount(responses)
   return (
-    <View style={style.chartContainer}>
-      {Object.keys(responses).map(item => {
-        const { percentage, count, label, imageURL } = responses[item]
-        const isPhotoChoice = Boolean(imageURL)
-        const pc = percentage.toFixed(2)
-        const pcFormatted = /\.00$/.test(pc) ? pc.match(/([0-9]+)/)[0] : pc // omit ".00"
+    <View onLayout={onLayout}>
+      <View style={style.chartContainer}>
+        {sortedKeys.map(item => {
+          const { percentage, count, label, imageURL } = responses[item]
+          const isPhotoChoice = Boolean(imageURL)
+          const pc = percentage.toFixed(2)
+          const pcFormatted = /\.00$/.test(pc) ? pc.match(/([0-9]+)/)[0] : pc // omit ".00"
 
-        const Photo = (
-          <Image
-            source={{ uri: imageURL }}
-            style={style.photo}
-          />
-        )
+          const Photo = (
+            <Image
+              source={{ uri: imageURL }}
+              style={style.photo}
+            />
+          )
 
-        const Percentage = (
-          <Text style={style.percentageComplete}>
-            {pcFormatted}%
-          </Text>
-        )
+          const Percentage = (
+            <Text style={style.percentageComplete}>
+              {pcFormatted}%
+            </Text>
+          )
 
-        const BarLabel = (
-          <Text style={style.barLabel}>
-            {label}
-          </Text>
-        )
+          const BarLabel = (
+            <Text style={style.barLabel}>
+              {label}
+            </Text>
+          )
 
-        return (
-          <View key={label} style={[style.row, style.barContainer]}>
-            <View>
-              {isPhotoChoice ? Photo : Percentage}
+          return (
+            <View key={label} style={[style.row, style.barContainer]}>
+              <View>
+                {isPhotoChoice ? Photo : Percentage}
+              </View>
+              <View style={[style.row, style.bar]}>
+                <View style={[style.barCompletion, { width: `${pcFormatted}%` }]}/>
+                {isPhotoChoice ? Percentage : BarLabel}
+                <Text style={[style.responseCount, style.responseCountNumber]}>
+                  {count}
+                </Text>
+                <Text style={style.responseCount}>
+                  {`response${(count > 1 ? 's' : '')}`.toUpperCase()}
+                </Text>
+              </View>
             </View>
-            <View style={[style.row, style.bar]}>
-              <View style={[style.barCompletion, { width: `${pcFormatted}%` }]} />
-              {isPhotoChoice ? Percentage : BarLabel}
-              <Text style={[style.responseCount, style.responseCountNumber]}>
-                {count}
-              </Text>
-              <Text style={style.responseCount}>
-                {`response${(count > 1 ? 's' : '')}`.toUpperCase()}
-              </Text>
-            </View>
-          </View>
-        )
-      })}
+          )
+        })}
+      </View>
     </View>
   )
 }
@@ -55,28 +70,15 @@ export default HorizontalBarChart
 
 const fontFamily = 'Apercu Pro'
 const style = StyleSheet.create({
-  questionTypeIcon: {
-    width: 20,
-    height: 10,
-    borderRadius: 10,
-    marginHorizontal: 4,
-    marginTop: 3
-  },
-  responseRate: {
-    fontSize: 12,
-    lineHeight: 18,
-    color: '#999',
-    fontFamily
-  },
   chartContainer: {
     marginTop: 8,
-    marginBottom: 4
+    marginBottom: 4,
+    height: 'auto'
   },
   row: {
-    flexDirection: 'row',
+    flexDirection: 'row'
   },
   barContainer: {
-    flex: 1,
     marginVertical: 4,
     alignItems: 'center'
   },
