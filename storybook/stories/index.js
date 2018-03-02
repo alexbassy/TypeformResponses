0e083693054800b48e069d438bd01f0b24b7f14a
Alex Bass <alexpjbass@gmail.com>
2018-03-02 23:06:29 +0100

    chore: Add picture choice block

--- storybook/stories/index.js
+++ storybook/stories/index.js
@@ -12,6 +12,29 @@ import { linkTo } from '@storybook/addon-links'
 import Block from '../../components/Block'
 
 storiesOf('Block', module)
+  .add('Picture Choice', () => {
+    const statement = form.fields[2]
+    const field = form.fields[3]
+    const fieldResponses = getResponsesForQuestion(field, responses)
+    const totalResponsesCount = responses.items.length
+
+    return (
+      <View style={styles.container}>
+        <ScrollView>
+          <Block
+            field={statement}
+            responses={fieldResponses}
+            totalResponsesCount={totalResponsesCount}
+          />
+          <Block
+            field={field}
+            responses={fieldResponses}
+            totalResponsesCount={totalResponsesCount}
+          />
+        </ScrollView>
+      </View>
+    )
+  })
   .add('Multiple Choice', () => {
     const field = form.fields[0]
     const fieldResponses = getResponsesForQuestion(field, responses)
