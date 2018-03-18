a09d8ee7cc0ad8089e2fcb0ae78668fbc0ba6473
Alex Bass <alexpjbass@gmail.com>
2018-03-18 15:24:37 +0100

    fix: fix stories

--- storybook/stories/block.example.js
+++ storybook/stories/block.example.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { View, StyleSheet, ScrollView } from 'react-native'
+import {View, StyleSheet, ScrollView} from 'react-native'
 
 import form from './fixtures/form.json'
 import responses from './fixtures/responses.json'
-import { getResponsesForQuestion } from '../../src/utils'
+import {getResponsesForQuestion} from '../../src/utils'
 
-import { storiesOf } from '@storybook/react-native'
+import {storiesOf} from '@storybook/react-native'
 
 import Block from '../../src/components/Block'
 
@@ -52,6 +52,31 @@ storiesOf('Block', module)
   })
   .add('Short Text', () => {
     const field = form.fields[1]
+    const fieldResponses = getResponsesForQuestion(field, responses)
+    const totalResponsesCount = responses.items.length
+
+    return (
+      <View style={styles.container}>
+        <ScrollView>
+          <Block
+            field={field}
+            responses={fieldResponses}
+            totalResponsesCount={totalResponsesCount}
+          />
+        </ScrollView>
+      </View>
+    )
+  })
+  .add('Piping', () => {
+    const field = {
+      'id': 'K9K88c5JkYy1',
+      'title': 'Si tienes cualquier problema, un humano podrá apoyarte en menos de 24 horas. Cómo te parece, *{{field:796bfe37-4e19-4746-b790-162ce4086220}}*?',
+      'ref': '467af588-ab34-4db5-984c-a9bf698f467d',
+      'validations': {
+        'required': false
+      },
+      'type': 'short_text'
+    }
     const fieldResponses = getResponsesForQuestion(field, responses)
     const totalResponsesCount = responses.items.length
 
