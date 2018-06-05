a952ab3c5fef21d82c0c8d65d009e76ce2a0ba94
Alex Bass <alexpjbass@gmail.com>
2018-06-05 08:55:44 +0200

    refactor: start on parser

---
+++ src/util/QuestionParser/input-stream.js
@@ -0,0 +1,35 @@
+function InputStream (input) {
+  let pos = 0
+  let line = 1
+  let col = 0
+
+  return {
+    next: next,
+    peek: peek,
+    eof: eof,
+    croak: croak
+  }
+
+  function next () {
+    const ch = input.charAt(pos++)
+    if (ch === '\n') {
+      line++
+      col = 0
+    } else {
+      col++
+    }
+    return ch
+  }
+
+  function peek () {
+    return input.charAt(pos)
+  }
+
+  function eof () {
+    return peek() === ''
+  }
+
+  function croak (msg) {
+    throw new Error(msg + ' (' + line + ':' + col + ')')
+  }
+}
